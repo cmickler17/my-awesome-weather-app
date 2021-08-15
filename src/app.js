e97a21f9d0e84bdb04e8aec6f08ca30600709b95
cmickler17 <87788655+cmickler17@users.noreply.github.com>
2021-08-15 12:11:16 -0400

    Created API path to obtain forecast of searched city

--- src/app.js
+++ src/app.js
@@ -22,7 +22,8 @@ let day = days[date.getDay()];
 return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+    console.log(response.data.daily);
     let forecastElement = document.querySelector("#forecast");
     
     let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -54,6 +55,12 @@ function displayForecast() {
     forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+    let apiKey = "f5e66638242de3dc22bd15c331cb267e";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemp(response) {
     celsiusTemp = response.data.main.temp;
 
@@ -81,6 +88,8 @@ function displayTemp(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     );
     iconElement.setAttribute("alt", response.data.weather[0].description);
+
+    getForecast(response.data.coord);
 }
 
 function handleSubmit(event) {
@@ -124,4 +133,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemp);
 
 search("Charlotte");
-displayForecast();
