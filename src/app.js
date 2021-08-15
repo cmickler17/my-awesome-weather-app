42a9fc4efee7469458e446a43592c2f022f60457
cmickler17 <87788655+cmickler17@users.noreply.github.com>
2021-08-15 14:27:53 -0400

    Displayed the upcoming forecast with the API

--- src/app.js
+++ src/app.js
@@ -22,35 +22,44 @@ let day = days[date.getDay()];
 return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    return days[day];
+}
+
 function displayForecast(response) {
-    console.log(response.data.daily);
+    let forecast = response.data.daily;
     let forecastElement = document.querySelector("#forecast");
     
-    let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
     let forecastHTML = `<div class="row">`;
-    days.forEach(function (day) {
+    forecast.forEach(function (forecastDay, index) {
+        if (index < 6) {
+
     forecastHTML = forecastHTML +
 			`
             <div class="col-2">
-				<div class="weather-forecast-date">${day}</div>
+				<div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
 				<img 
-				src="http://openweathermap.org/img/wn/11d@2x.png"
+				src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
 				alt=""
 				width="42"
 				/>
 				    <div class="weather-forecast-temp">
 						<span class="weather-forecast-temp-max">
-						18°
+						${Math.round(forecastDay.temp.max)}°
 						</span>
 						<span class="weather-forecast-temp-min">
-						12°
+						${Math.round(forecastDay.temp.min)}°
 						</span>
 					</div>
 			</div>
         `;
+            }
         });
-    
+
     forecastHTML = forecastHTML + `</div>`;
     forecastElement.innerHTML = forecastHTML;
 }
